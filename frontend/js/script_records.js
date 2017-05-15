f13707d04e93a81509d526c938d57715f3669ce0
urzp <urzp002@mail.ru>
2017-05-15 19:56:55 +0300

    divase records and game

---
+++ frontend/js/script_records.js
@@ -0,0 +1,24 @@
+$('dicument').ready(function(){
+    
+
+    
+    $('.row>img').on("click",function(){
+
+        var img = $(this).attr('src');
+        
+        $('.board>img').attr('src',img);
+         var options = { percent: 10 };
+         $( ".board" ).show( "scale" , options ,500 );
+        
+    });
+    
+    $('body').on("keydown",function(event){
+       if (event.which == 27){
+          $( ".board" ).removeAttr( "style" ).fadeOut(); 
+       };
+    });
+    
+
+ 
+})
+   
