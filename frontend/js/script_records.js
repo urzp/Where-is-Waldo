a62f59896a3fff8f45f0937981df3409d4fc6c34
urzp <urzp002@mail.ru>
2017-05-17 10:38:21 +0300

    show hide records tables

--- frontend/js/script_records.js
+++ frontend/js/script_records.js
@@ -1,20 +1,24 @@
 $('dicument').ready(function(){
     
 
-    
+    var options = { percent: 10 };
     $('.row>img').on("click",function(){
 
-        var img = $(this).attr('src');
+        //var img = $(this).attr('src');
         
-        $('.board>img').attr('src',img);
-         var options = { percent: 10 };
-         $( ".board" ).show( "scale" , options ,500 );
+        //$('.board>img').attr('src',img);
+         
+         $( ".main" ).hide( "scale" , options ,500 );  
+         $( ".table" ).show( "scale" , options ,500 );
         
     });
     
     $('body').on("keydown",function(event){
        if (event.which == 27){
-          $( ".board" ).removeAttr( "style" ).fadeOut(); 
+           
+          $( ".main" ).show( "scale" , options ,500 ); 
+          $( ".table" ).removeAttr( "style" ).fadeOut(); 
+          
        };
     });
     
