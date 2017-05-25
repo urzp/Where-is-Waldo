140a64c564cf5e5aff21118c9f632d4a8d954941
urzp <urzp002@mail.ru>
2017-05-25 14:45:00 +0300

    add id_board to ajax

--- backend/Where_Is_Waldo/app/assets/javascripts/games.js
+++ backend/Where_Is_Waldo/app/assets/javascripts/games.js
@@ -5,8 +5,11 @@ $('dicument').ready(function(){
     $('.games>.row>img').on("click",function(){
 
         var img = $(this).attr('src');
+        var board_id = $(this).parent().attr('id')
+
 
         $('.board>img').attr('src',img);
+        $('.board').attr("id",board_id);
          var options = { percent: 10 };
          $( ".board" ).show( "scale" , options ,500 );
 
@@ -21,10 +24,13 @@ $('dicument').ready(function(){
     $(".board>img").click(function(e){
       var xClick = e.pageX - $(this).offset().left;
       var yClick = e.pageY - $(this).offset().top;
+      
+      var board_id = $(".board").attr("id")
+
       $.ajax({
         url: 'target',
         type: 'GET',
-        data: { xClick: xClick, yClick: yClick }
+        data: {board_id: board_id , xClick: xClick, yClick: yClick }
       })
     });
 
